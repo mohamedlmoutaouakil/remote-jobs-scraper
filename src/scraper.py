54b1d649a8b632018b39d06f7cbe61f6712752a6
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-04-10 14:13:52 -0600

    Added job location job results & main function

--- src/scraper.py
+++ src/scraper.py
@@ -20,6 +20,7 @@ class Scraper:
             job_title_elem = job_post.find('h2')
             company_name_header = job_post.find('h3')
             company_name_elem = company_name_header.find('span')
+            location_elem = company_name_header.find_all('span')[-1]
             technologies_elements = job_post.find_all('a', class_=['post-tag', 'no-tag-menu'])
             if None in (job_title_elem, company_name_elem, technologies_elements):
                 continue
@@ -27,17 +28,26 @@ class Scraper:
             job_post_dict = {}
             job_post_dict['job_title'] = job_title_elem.text.strip()
             job_post_dict['company_name'] = company_name_elem.text.strip()
+            job_post_dict['location'] = location_elem.text.strip()
             job_post_dict['technologies_list'] = [elem.text.strip() for elem in technologies_elements]
             jobs_list.append(job_post_dict)
         
         return jobs_list
 
+def main():
+    print('Welcome to remote jobs scraper!')
+    search_input = input('Search for job : ')
+    keywords = search_input.split(' ')
+    # initialize URL
+    URL = 'https://stackoverflow.com/jobs'
+    if len(keywords) > 0 :
+        q_parameter = '+'.join(keywords)
+        URL = URL + '?q=' + q_parameter + '&r=true' # r=true to search for Remote jobs only
+    # instanciate scraper class
+    scraper = Scraper(URL)
+    jobs_list = scraper.get_jobs_list()
+    pp = pprint.PrettyPrinter()
+    pp.pprint(jobs_list)
 
-
-
-URL = 'https://stackoverflow.com/jobs?q=Software+Developer&r=true&c=usd&mxs=Junior'
-scraper = Scraper(URL)
-jobs_list = scraper.get_jobs_list()
-
-pp = pprint.PrettyPrinter()
-pp.pprint(jobs_list)
+if __name__ == "__main__":
+    main()
